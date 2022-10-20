7a35df7423602cb9a4e5de6586c06127cf8c7703
liuchu <liuchuu@126.com>
2022-10-20 14:34:26 +0800

    test: OperatingSystemMXBean

--- src/main/java/priv/liuchu/wechat/demo/WechatDemoApplication.java
+++ src/main/java/priv/liuchu/wechat/demo/WechatDemoApplication.java
@@ -3,10 +3,31 @@ package priv.liuchu.wechat.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.lang.management.ManagementFactory;
+import java.lang.management.OperatingSystemMXBean;
+
 @SpringBootApplication
 public class WechatDemoApplication {
 
 	public static void main(String[] args) {
+
+		new Thread(() -> {
+
+			while (true) {
+				OperatingSystemMXBean operatingSystemMXBean = ManagementFactory.getOperatingSystemMXBean();
+
+				double systemLoad = operatingSystemMXBean.getSystemLoadAverage();
+				System.out.println(systemLoad);
+
+				try {
+					Thread.sleep(60000);
+				} catch (InterruptedException e) {
+					Thread.currentThread().interrupt();
+				}
+			}
+
+		}).start();
+
 		SpringApplication.run(WechatDemoApplication.class, args);
 	}
 
